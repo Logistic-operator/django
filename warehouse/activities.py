ece61ce907dea0e33316320a681d5ec18e5b7181
oldi17 <odi.scs.15@gmail.com>
2024-01-04 15:22:25 +0000

    bugfix

--- warehouse/activities.py
+++ warehouse/activities.py
@@ -29,6 +29,9 @@ async def findNearestStation(wh_id) -> str:
 @activity.defn
 async def create(input: ComposeCreateInput) -> int:
     Warehouse = apps.get_model(app_label='warehouse', model_name='Warehouse')
+    old = await Warehouse.objects.filter(point=input.point).afirst()
+    if old:
+        raise AttributeError('Already Exists!')
     the_wh = Warehouse(phone=input.phone, point=input.point)
     await the_wh.asave()
     return the_wh.id
